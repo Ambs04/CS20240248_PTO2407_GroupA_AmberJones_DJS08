1d47218d22bde679f225525d86cad199b3b7e7e4
Ambs04 <amberleejones2007@gmail.com>
2025-05-01 21:12:40 +0200

    create Route structure with nav bar structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { BrowserRouter, Routes, Route  } from "react-router-dom"
-import Home from "./Pages/Home"
-import About from "./Pages/About"
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
 
 function App() {
-
   return (
-    
-  )
+    <BrowserRouter>
+      <header>
+        <nav></nav>
+      </header>
+      <Routes>
+        <Route />
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
