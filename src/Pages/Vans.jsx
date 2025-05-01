2f28fcfde23c53ce67ab2792d4ce2a647acefaf3
Ambs04 <amberleejones2007@gmail.com>
2025-05-01 22:43:41 +0200

    fetch data from 'api' and apply custom cards to each van from map

--- src/Pages/Vans.jsx
+++ src/Pages/Vans.jsx
@@ -1,6 +1,28 @@
-//import React from "react";
+import React from "react";
 import "../server";
 
 export default function Vans() {
+  //set state(empty array) to a variable
+  const [vans, setVans] = React.useState([]);
+
+  //fetch data from server.js 'api' and load api data once
+  React.useEffect(() => {
+    fetch("../server/vans")
+      .then((res) => res.json())
+      .then((data) => setVans(data.vans));
+  }, []);
+
+  //map over api data and log a custom card for each van in the react dom
+  const vanEl = vans.map((van) => (
+    <div key={van.id}>
+      <img src={van.imageUrl} />
+      <div>
+        <h3>{van.name}</h3>
+        <p>`${van.price}/day`</p>
+      </div>
+      <i>{van.type}</i>
+    </div>
+  ));
+
   return <h1>Vans go here</h1>;
 }
