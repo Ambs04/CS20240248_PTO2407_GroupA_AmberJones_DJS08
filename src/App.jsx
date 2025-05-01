f6cb8d249ce394def571a7e435e19c74fc6a2125
Ambs04 <amberleejones2007@gmail.com>
2025-05-01 22:23:27 +0200

    import sever.js data from scrimba

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Vans from "./Pages/Vans";
+import "../src/server";
 
 function App() {
   return (
