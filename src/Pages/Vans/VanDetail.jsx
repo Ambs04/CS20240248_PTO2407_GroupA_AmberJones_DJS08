6e12783a065baa39f77f058959b1b9fd48c3e1ed
Ambs04 <amberleejones2007@gmail.com>
2025-05-02 16:13:36 +0200

    set return info fro the van detail component

--- src/Pages/Vans/VanDetail.jsx
+++ src/Pages/Vans/VanDetail.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useParams } from "react-router-dom";
 
 export default function VanDetail() {
@@ -14,4 +15,23 @@ export default function VanDetail() {
       .then((data) => setVan(data.vans));
     //fetch data each time the path id changes
   }, [params.id]);
+
+  //Use ternary to see if van exists, if it does, display van  detail tile. If it doesn't exist then render the h2 text
+
+  return (
+    <div>
+      {van ? (
+        <div>
+          <img src={van.imageUrl} />
+          <i>{van.type}</i>
+          <h2>{van.name}</h2>
+          <p></p>
+          <p>{van.description}</p>
+          <button>Rent this van</button>
+        </div>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
 }
