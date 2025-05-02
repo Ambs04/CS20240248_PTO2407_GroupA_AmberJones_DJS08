52a5e8a5f3cd5c7950cb3a6f1be9ad0259e593bc
Ambs04 <amberleejones2007@gmail.com>
2025-05-02 19:13:08 +0200

    create styles object and use boolean to see if link is active

--- src/Components/HostLayout.jsx
+++ src/Components/HostLayout.jsx
@@ -2,10 +2,21 @@
 import { NavLink, Outlet } from "react-router-dom";
 
 export default function HostLayout() {
+  //object containing styles
+  const styleActive = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
+
   return (
     <>
       <nav className="host-nav">
-        <NavLink to="." end>
+        <NavLink
+          to="."
+          end
+          style={({ isActive }) => (isActive ? styleActive : null)}
+        >
           Dashboard
         </NavLink>
         <NavLink to="income">Income</NavLink>
