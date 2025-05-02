7347779316637ffd9bcb98f628ea8f80aedb8cf2
Ambs04 <amberleejones2007@gmail.com>
2025-05-02 19:42:19 +0200

    create footer

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,13 +1,15 @@
 import Header from "./Header";
+import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div>
+    <div className="site-wrapper">
       <Header />
       <main>
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 }
