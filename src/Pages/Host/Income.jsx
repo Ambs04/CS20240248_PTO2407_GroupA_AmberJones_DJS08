4271a00abb01cc85b428d6670a80c26529dfd145
Ambs04 <amberleejones2007@gmail.com>
2025-05-04 12:50:38 +0200

    creaet income page

--- src/Pages/Host/Income.jsx
+++ src/Pages/Host/Income.jsx
@@ -1,3 +1,28 @@
 export default function Income() {
-  return <h1>Income content</h1>;
+  const transactions = [
+    { amount: 720, date: "Jan 3, '25", id: "1" },
+    { amount: 560, date: "Dec 4, '24", id: "2" },
+    { amount: 980, date: "Dec 27, '24", id: "3" },
+  ];
+
+  return (
+    <section className="host-income">
+      <h1>Income</h1>
+      <p>Last 30 days</p>
+      <h2>$2,260</h2>
+      <img src=".././public/Images/income-graph.png" className="graph" />
+      <div className="info-header">
+        <h3>Your transactions (3)</h3>
+        <p>Last 30 days</p>
+      </div>
+      <div className="transactions">
+        {transactions.map((item) => (
+          <div key={item.id} className="transaction">
+            <h3>${item.amount}</h3>
+            <p>{item.date}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
