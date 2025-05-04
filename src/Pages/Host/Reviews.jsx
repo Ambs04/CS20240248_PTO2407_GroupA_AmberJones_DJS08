8ab425598e486482a4b878ef1cdfd978b0354cfd
Ambs04 <amberleejones2007@gmail.com>
2025-05-04 12:40:58 +0200

    create reviews page

--- src/Pages/Host/Reviews.jsx
+++ src/Pages/Host/Reviews.jsx
@@ -1,3 +1,46 @@
+import React from "react";
+
 export default function Reviews() {
-  return <h1>Reviews here</h1>;
+  const reviewData = [
+    {
+      rating: 5,
+      name: "Elliot",
+      date: "January 3, 2023",
+      text: "The beach bum is such an awesome van! Such a comfortable trip. We had it for 2 weeks and there was not a single issue. Super clean when we picked it up and the host is very comfortable and understanding. Highly recommend!",
+      id: "1",
+    },
+    {
+      rating: 5,
+      name: "Sandy",
+      date: "December 12, 2022",
+      text: "This is our third time using the Modest Explorer for our travels and we love it! No complaints, absolutely perfect!",
+      id: "2",
+    },
+  ];
+
+  return (
+    <section className="host-reviews">
+      <div className="top-text">
+        <h2>Your reviews</h2>
+        <p>Last 30 days</p>
+      </div>
+      <img src=".././public/Images/reviews-graph.png" className="graph" />
+      <h3>Reviews (2)</h3>
+      {reviewData.map((review) => (
+        <div key={review.id}>
+          <div>
+            {[...Array(review.rating)].map((_, i) => (
+              <div className="review-star" key={i}></div>
+            ))}
+            <div className="info">
+              <p className="name">{review.name}</p>
+              <p className="date">{review.date}</p>
+            </div>
+            <p>{review.text}</p>
+          </div>
+          <hr />
+        </div>
+      ))}
+    </section>
+  );
 }
