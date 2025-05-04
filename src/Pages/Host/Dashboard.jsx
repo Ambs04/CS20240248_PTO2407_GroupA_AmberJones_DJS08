ac3530018b84cfa6d6fc6df98dfb23dec6b95592
Ambs04 <amberleejones2007@gmail.com>
2025-05-04 13:24:54 +0200

    complete dashboard component

--- src/Pages/Host/Dashboard.jsx
+++ src/Pages/Host/Dashboard.jsx
@@ -1,29 +1,55 @@
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import React from "react";
+import "../../server";
 
 export default function Dashboard() {
-  // const [vans, setVans] = React.useState([]);
+  const [vans, setVans] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("/api/host/vans")
+      .then((res) => res.json())
+      .then((data) => setVans(data.vans));
+  }, []);
+
+  function vanElRender(vans) {
+    const vanEl = vans.map((van) => (
+      <div className="host-van-single" key={van.id}>
+        <img src={van.imageUrl} />
+        <div className="host-van-info">
+          <h3>{van.name}</h3>
+          <p>${van.price} / day</p>
+        </div>
+        <Link to={`vans/${van.id}`}>View</Link>
+      </div>
+    ));
+    return (
+      <div className="host-vans-list">
+        <section>{vanEl}</section>
+      </div>
+    );
+  }
 
   return (
     <>
-      <section>
-        <div>
-          <h1></h1>
-          <p></p>
-          <h2></h2>
+      <section className="host-dashboard-earnings">
+        <div className="info">
+          <h1>Welcome!</h1>
+          <p>Income last 30 days</p>
+          <h2>$2,260</h2>
         </div>
-        <link to="income">Details</link>
+        <Link to="income">Details</Link>
       </section>
-      <section>
+      <section className="host-dashboard-reviews">
         <h2></h2>
-        <p></p>
-        <link to="reviews">Details</link>
+        <p>5.0 / 5</p>
+        <Link to="reviews">Details</Link>
       </section>
-      <section>
-        <div>
-          <h2></h2>
-          <link to="vans">View all</link>
+      <section className="host-dashboard-vans">
+        <div className="top">
+          <h2>Your listed vans</h2>
+          <Link to="vans">View all</Link>
         </div>
-        <div></div>
+        {vanElRender(vans)}
       </section>
     </>
   );
